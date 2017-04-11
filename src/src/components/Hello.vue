ae592e84096dee5b40c075aece5b2e857159ad00
wub <wub@landicorp.com>
2017-04-11 15:48:14 +0800

    Complete layout.

--- src/src/components/Hello.vue
+++ src/src/components/Hello.vue
@@ -1,53 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+  <div class="todo-list">
+    <div class="form-group">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Input new TODO item">
+        <span class="input-group-btn">
+            <button type="button" class="btn btn-default" aria-label="Add">
+              <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+            </button>
+        </span>
+      </div>
+    </div>
+    <ul class="list-group">
+      <a href="#" class="list-group-item active">
+          Todo Items
+        </a>
+      <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+      <a href="#" class="list-group-item">Morbi leo risus</a>
+      <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+      <a href="#" class="list-group-item">Vestibulum at eros</a>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <ul class="list-group">
+      <a href="#" class="list-group-item list-group-item-success">
+          Done Items
+        </a>
+      <a href="#" class="list-group-item text-muted todo-item-done">Dapibus ac facilisis in</a>
+      <a href="#" class="list-group-item text-muted todo-item-done">Morbi leo risus</a>
+      <a href="#" class="list-group-item text-muted todo-item-done">Porta ac consectetur ac</a>
+      <a href="#" class="list-group-item text-muted todo-item-done">Vestibulum at eros</a>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
-};
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      };
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  .todo-list {
+    padding: 10px;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .todo-item-done {
+    text-decoration:line-through
+  }
 </style>
